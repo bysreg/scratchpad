baf4245b33905abe1f9ee369b0aa6c94c6d9df8d
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2017-02-25 22:53:31 -0500

    runnable with 64 bit dll

--- qt_with_cmake/main.cpp
+++ qt_with_cmake/main.cpp
@@ -1,5 +1,13 @@
 #include "mainwindow.h"
+
+#include <GL/glew.h>
+#include <SDL.h>
+
 #include <QApplication>
+
+#define BABI_OGL_VERSION 330
+#define BABI_OGL_MAJOR_VERSION 3
+#define BABI_OGL_MINOR_VERSION 3
 
 int main(int argc, char *argv[])
 {
