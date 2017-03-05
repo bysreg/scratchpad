6271c41811a987a7a84b061de5f2854d076799dc
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2017-03-04 21:52:31 -0500

    make openglwidget inside main window

--- qt_with_cmake/main.cpp
+++ qt_with_cmake/main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-#include "projectwindow.hpp"
+//#include "projectwindow.hpp"
 #include "openglwidget.hpp"
 
 #include <QApplication>
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 
 	QSurfaceFormat format;
 	format.setSamples(16);
+
 	format.setMajorVersion(3);
 	format.setMinorVersion(3);
 	format.setProfile(QSurfaceFormat::CoreProfile);
@@ -21,13 +22,13 @@ int main(int argc, char *argv[])
 	//window.show();
 	//window.setAnimating(true);
 
-	MainWindow main_window;
-	main_window.show();
+	//MainWindow main_window;
+	//main_window.show();
 
-	//OpenGLWidget glwidget(nullptr);
-	//glwidget.setFormat(format);
-	//glwidget.resize(1024, 768);
-	//glwidget.show();
+	OpenGLWidget glwidget(nullptr);
+	glwidget.setFormat(format);
+	glwidget.resize(1024, 768);
+	glwidget.show();
 
 	return app.exec();
 }
