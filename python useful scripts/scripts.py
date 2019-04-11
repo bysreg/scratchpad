3fcad14485c59a1b5029e8f8bf5d4829ac6e417b
Hilman Beyri <hilmanbeyri@gmail.com>
2019-04-11 16:06:37 -0400

    add scripts.py

---
+++ python useful scripts/scripts.py
@@ -0,0 +1,15 @@
+
+'''
+	replace every files in the current folder with new name from replacement
+
+'''
+def re_replace(pattern, replacement):
+    import glob
+    import re
+    import os
+    for filename in glob.glob('*'):
+        new_name = re.sub(pattern, replacement, filename)
+        if not dry_run:
+            os.rename(filename, new_name)
+        else:
+            print(new_name)
