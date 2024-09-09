33f95140863d3f1ba55456844e562919cef7d893
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2024-09-09 13:55:22 -0400

    new python function

--- python useful scripts/scripts.py
+++ python useful scripts/scripts.py
@@ -1,17 +1,21 @@
-
-'''
-    replace every files in the current folder with new name from replacement
-
-'''
+import glob
+import re
+import os
 
 
 def re_replace(pattern, replacement, dry_run):
-    import glob
-    import re
-    import os
+    '''Replace every files in the current folder with new name '''
     for filename in glob.glob('*'):
         new_name = re.sub(pattern, replacement, filename)
         if not dry_run:
             os.rename(filename, new_name)
         else:
             print(new_name)
+
+
+def get_file_names():
+    ''' return all file names in the current folder separated by ";". '''
+    ret = ""
+    for filename in glob.glob('*'):
+        ret += filename + ";"
+    return ret
